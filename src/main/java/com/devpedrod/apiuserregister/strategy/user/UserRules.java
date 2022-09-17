9b95f6d8072ed9cf57bc75987e135f2027631ecb
João Pedro <pedroolivxira@gmail.com>
2022-09-17 17:45:33 -0300

    feat(Strategy): Add exception to user rules

--- src/main/java/com/devpedrod/apiuserregister/strategy/user/UserRules.java
+++ src/main/java/com/devpedrod/apiuserregister/strategy/user/UserRules.java
@@ -2,26 +2,35 @@ package com.devpedrod.apiuserregister.strategy.user;
 
 import com.devpedrod.apiuserregister.domain.DomainEntity;
 import com.devpedrod.apiuserregister.domain.User;
+import com.devpedrod.apiuserregister.dto.response.FieldMessage;
+import com.devpedrod.apiuserregister.exceptions.MethodArgumentNotValidException;
 import com.devpedrod.apiuserregister.strategy.IStrategy;
 import com.devpedrod.apiuserregister.utils.CpfValidator;
 import com.devpedrod.apiuserregister.utils.NameValidator;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserRules implements IStrategy {
     @Override
     public DomainEntity applyBusinessRule(DomainEntity domainEntity) {
+        List<FieldMessage> fieldMessages = new ArrayList<>();
         if (domainEntity instanceof User user) {
             user.setName(user.getName().trim());
             user.setCpf(user.getCpf().trim().replaceAll("[.-]", ""));
 
             if (!CpfValidator.isValidCPF(user.getCpf())){
-                // TODO: CPF Exception
-                System.out.println("CPF INVALIDO");
-            } if (!NameValidator.validateName(user.getName())) {
-                // TODO: Name Exception
-                System.out.println("NOME INVALIDO");
+                fieldMessages.add(new FieldMessage("CPF","CPF ínvalido"));
             }
+            if (!NameValidator.validateName(user.getName())) {
+                fieldMessages.add(new FieldMessage("Nome","Nome ínvalido, use somente letras."));
+            }
+            if (!fieldMessages.isEmpty()){
+                throw new MethodArgumentNotValidException(fieldMessages);
+            }
+
             return user;
         }
         return null;
