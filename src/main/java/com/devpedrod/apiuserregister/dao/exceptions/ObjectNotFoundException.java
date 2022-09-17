3e0a0bb87068f8080e054dd693aa67df24389643
João Pedro <pedroolivxira@gmail.com>
2022-09-17 14:03:45 -0300

    feat(Exceptions): Object not found exception

---
+++ src/main/java/com/devpedrod/apiuserregister/dao/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.devpedrod.apiuserregister.dao.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+    public ObjectNotFoundException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
