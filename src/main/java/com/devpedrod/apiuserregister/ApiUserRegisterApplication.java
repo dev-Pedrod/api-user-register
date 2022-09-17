5dc6d764b90a61c1cd288b288ebdc1eb54fa129c
João Pedro <pedroolivxira@gmail.com>
2022-09-17 07:51:47 -0300

    project created

---
+++ src/main/java/com/devpedrod/apiuserregister/ApiUserRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.devpedrod.apiuserregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpedrod/apiuserregister/ApiUserRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpedrod.apiuserregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
