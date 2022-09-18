90c99a3a81025f9482e81ed0123274e432b49b90
João Pedro <pedroolivxira@gmail.com>
2022-09-18 12:15:03 -0300

    feat(User): Delete, Disable and Update User

--- src/main/java/com/devpedrod/apiuserregister/controllers/UserController.java
+++ src/main/java/com/devpedrod/apiuserregister/controllers/UserController.java
@@ -5,12 +5,15 @@ import com.devpedrod.apiuserregister.domain.User;
 import com.devpedrod.apiuserregister.dto.user.UserDto;
 import com.devpedrod.apiuserregister.strategy.user.UserRules;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,13 +27,33 @@ public class UserController {
     private ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<Void> createUser(@RequestBody @Valid User user){
         userDAO.save(user, strategy::applyBusinessRule);
         return ResponseEntity.created(ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(user.getId())
                 .toUri()).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user){
+        user.setId(id);
+        BeanUtils.copyProperties(user, userDAO.getById(id));
+        userDAO.update(user, strategy::applyBusinessRule);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id){
+        userDAO.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/disable/{id}")
+    public ResponseEntity<User> disableUser(@PathVariable Long id){
+        userDAO.disable(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
