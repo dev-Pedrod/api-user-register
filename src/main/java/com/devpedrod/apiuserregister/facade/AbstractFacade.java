85b06948486387fbc53cffafaffda1381c5efe8e
João Pedro <pedroolivxira@gmail.com>
2022-09-18 18:17:48 -0300

    feat(Facade): Abstract facade to User and Address

---
+++ src/main/java/com/devpedrod/apiuserregister/facade/AbstractFacade.java
@@ -0,0 +1,59 @@
+package com.devpedrod.apiuserregister.facade;
+
+import com.devpedrod.apiuserregister.dao.impl.AddressDAO;
+import com.devpedrod.apiuserregister.dao.impl.GenericDAO;
+import com.devpedrod.apiuserregister.dao.impl.UserDAO;
+import com.devpedrod.apiuserregister.domain.Address;
+import com.devpedrod.apiuserregister.domain.User;
+import com.devpedrod.apiuserregister.strategy.IStrategy;
+import com.devpedrod.apiuserregister.strategy.address.AddressStrategy;
+import com.devpedrod.apiuserregister.strategy.user.UserDisableStrategy;
+import com.devpedrod.apiuserregister.strategy.user.UserStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public abstract class AbstractFacade {
+    protected Map<String, GenericDAO> daos = new HashMap<>();
+    protected  Map<String, Map<String, List<IStrategy>>> businessRule = new HashMap<>();
+
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private UserStrategy userStrategy;
+    @Autowired
+    private UserDisableStrategy disableStrategy;
+
+    @Autowired
+    private AddressDAO addressDAO;
+    @Autowired
+    private AddressStrategy addressStrategy;
+
+    public void initialize(){
+        //----------------------- User --------------------------//
+        daos.put(User.class.getName(), userDAO);
+
+        List<IStrategy> rulesUser = new ArrayList<>(Collections.singleton(userStrategy));
+        List<IStrategy> rulesDisableUser = new ArrayList<>(Collections.singleton(disableStrategy));
+
+        Map<String,List<IStrategy>> mapKeyUser = new HashMap<>();
+        mapKeyUser.put("SAVE", rulesUser);
+        mapKeyUser.put("DISABLE", rulesDisableUser);
+
+        businessRule.put(User.class.getName(), mapKeyUser);
+
+        //----------------------- Address --------------------------//
+        daos.put(Address.class.getName(), addressDAO);
+
+        List<IStrategy> rulesAddress = new ArrayList<>(Collections.singleton(addressStrategy));
+
+        Map<String,List<IStrategy>> mapKeyAddress = new HashMap<>();
+        mapKeyAddress.put("SAVE", rulesAddress);
+
+        businessRule.put(Address.class.getName(), mapKeyAddress);
+    }
+
+}
+
