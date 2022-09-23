c4146a781480e6e4d4baf68696ed1825dc8c677d
João Pedro <pedroolivxira@gmail.com>
2022-09-23 09:18:31 -0300

    feat(user): Find user by CPF

--- src/main/java/com/devpedrod/apiuserregister/dao/IUserDAO.java
+++ src/main/java/com/devpedrod/apiuserregister/dao/IUserDAO.java
@@ -3,4 +3,5 @@ package com.devpedrod.apiuserregister.dao;
 import com.devpedrod.apiuserregister.domain.User;
 
 public interface IUserDAO extends IGenericDAO<User>{
+    User getByCpf(String cpf);
 }
