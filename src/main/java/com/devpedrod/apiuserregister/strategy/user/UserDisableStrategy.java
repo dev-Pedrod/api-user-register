284d6e931e79040952e965e86829c6858116089b
João Pedro <pedroolivxira@gmail.com>
2022-09-23 08:13:01 -0300

    fix(Strategy): Fix disable user if address is null

--- src/main/java/com/devpedrod/apiuserregister/strategy/user/UserDisableStrategy.java
+++ src/main/java/com/devpedrod/apiuserregister/strategy/user/UserDisableStrategy.java
@@ -12,7 +12,9 @@ public class UserDisableStrategy implements IStrategy {
     @Override
     public DomainEntity applyBusinessRule(DomainEntity domainEntity) {
         if (domainEntity instanceof User user) {
-            user.getAddress().setDisabledAt(now());
+            if(user.getAddress() != null) {
+                user.getAddress().setDisabledAt(now());
+            }
             user.getPermissions().parallelStream().forEach(permission -> permission.setDisabledAt(now()));
             user.getFormations().parallelStream().forEach(formation-> formation.setDisabledAt(now()));
         }
